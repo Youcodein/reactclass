da22dc181b5722db3416547ae3ba7a5c92b9f268
HWANGYoujin <ghkddbwls96@gmail.com>
2022-11-03 06:40:52 +0900

    221103

--- src/components/section/Card.js
+++ src/components/section/Card.js
@@ -1,15 +1,90 @@
 import React from "react";
 
+const cardTitle = {
+  title: "How?",
+  desc: "어떤 식으로 당신의 다이어리를 꾸밀건가요? 당신만의 멋진 다이어리를 만들어보세요!",
+};
+
+const cardContents = [
+  {
+    imgSrc:"card_bg01_01",
+    cardTit: "멋진 글을 적어보세요.",
+    cardDesc:"어떠한 글이 멋진 글이라고 정확히 정할 수는 없습니다만 당신이 쓰는 글이라면 어떤 글이든 멋질 거예요! 연필을 들고 종이에 적어볼까요? 용기를 내세요 글을 적으세요! 당신의 마음을 적어보세요!",
+
+  },
+  {
+    imgSrc:"card_bg01_02",
+    cardTit: "당신의 추억을 기록하세요!",
+    cardDesc:"기억으로 남기는 것도 좋은 방법이지만 기록으로 남기지 않은 기억은 금방 휘발되는 법이죠. 당신의 기억과 감정을 어딘가에 박제하세요. 훗날 당신의 추억을 읽고 기뻐할 당신의 얼굴을 상상하세요!",
+
+  },
+  {
+    imgSrc:"card_bg01_03",
+    cardTit: "당신의 추억을 장식하세요",
+    cardDesc:"당신의 감정을 시각적으로 표현하고 싶지 않으신가요? 스티커를 붙이세요. 그림을 그리세요. 당신의 추억을 장식하세요. 적절한 장식은 감정을 실감나게 표현해줍니다. 마음에 드는 스티커나 그림으로 예쁘게 장식해보세요",
+
+  },
+]
+
+function CardInner ({imgSrc, cardTit, cardDesc}) {
+  return (
+    <article className="card">
+          <figure className="card__header">
+            <img
+              src={`assets/img/${imgSrc}.jpg`}
+              alt="멋진 글을 적어보세요."
+            ></img>
+          </figure>
+          <div className="card__body">
+            <h3 className="tit">{cardTit}</h3>
+            <p className="desc">
+              {cardDesc}
+            </p>
+            <a className="btn" href="/">
+              더 자세히 보기
+              <span>
+                <svg
+                  width="65"
+                  height="8"
+                  viewBox="0 0 65 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M64.3536 4.35355C64.5488 4.15829 64.5488 3.84171 64.3536 3.64645L61.1716 0.464466C60.9763 0.269204 60.6597 0.269204 60.4645 0.464466C60.2692 0.659728 60.2692 0.976311 60.4645 1.17157L63.2929 4L60.4645 6.82843C60.2692 7.02369 60.2692 7.34027 60.4645 7.53553C60.6597 7.7308 60.9763 7.7308 61.1716 7.53553L64.3536 4.35355ZM0 4.5H64V3.5H0V4.5Z"
+                    fill="black"
+                  />
+                </svg>
+              </span>
+            </a>
+          </div>
+        </article>
+  )
+}
+
+
 function Card({ attr }) {
   return (
     <section id="cardType" className={`card__wrap ${attr[0]} ${attr[1]}`}>
-      <h2>How?</h2>
+      <h2>{cardTitle.title}</h2>
+      <p>
+      {cardTitle.desc}
+      </p>
+      {/* <h2>How?</h2>
       <p>
         어떤 식으로 당신의 다이어리를 꾸밀건가요? 당신만의 멋진 다이어리를
         만들어보세요!
-      </p>
+      </p> */}
       <div className={`card__inner ${attr[2]}`}>
-        <article className="card">
+        {cardContents.map((info) => (
+          <CardInner 
+          imgSrc={info.imgSrc}
+          cardTit={info.cardTit}
+          cardDesc={info.cardDesc}
+          />
+          ))
+          }
+        {/* <article className="card">
           <figure className="card__header">
             <img
               src="assets/img/card_bg01_01.jpg"
@@ -110,7 +185,7 @@ function Card({ attr }) {
               </span>
             </a>
           </div>
-        </article>
+        </article> */}
       </div>
     </section>
   );
--- src/components/section/ImgText.js
+++ src/components/section/ImgText.js
@@ -1,19 +1,95 @@
 import React from "react";
 
-function ImgText({ attr }) {
+const imgTextTitle = {
+  subTitle: "초보 다.꾸인을 위해",
+  title : "유용한 사이트 살펴보기",
+  desc: "다이어리 꾸미기를 처음 할 때 입문용으로 유용한 사이트입니다.",
+};
+
+const imgTextDesc = [
+  {
+    link: "/",
+    text: "튜토리얼 사이트",
+  },
+  {
+    link: "/",
+    text: "레퍼런스 사이트",
+  },
+  {
+    link: "/",
+    text: "디지털 다이어리 스티커",
+  },
+  {
+    link: "/",
+    text: "유용한 블로그 모음",
+  },
+  {
+    link: "/",
+    text: "다꾸 자랑 사진들",
+  },
+  {
+    link: "/",
+    text: "Youtube 사이트",
+  },
+];
+
+function ImgTextInner({link, text}){
+  return(
+    <li>
+      <a href={`${link}`}>{text}</a>
+    </li>
+  )
+}
+
+const imgTextBtn = [
+  {
+    btnClassName: "imgText__img",
+    imgClassName: "img1",
+    btnLink: "/",
+    linkText: "레퍼런스 사이트"
+  },
+  {
+    btnClassName: "imgText__img",
+    imgClassName: "img2",
+    btnLink: "/",
+    btnClass: "blue",
+    linkText: "튜토리얼 사이트"
+  },
+]
+
+function ImgTextInner2({btnClassName, imgClassName, btnLink, btnClass, linkText}){
+  return(
+    <div className={`${btnClassName} ${imgClassName}`}>
+      <a href={`${btnLink}`} className={`${btnClass}`}>{linkText}</a>
+    </div>
+  )
+}
+
+function ImgText(props) {
   return (
     <section
       id="imgTextType"
-      className={`imgText__wrap scroll ${attr[0]} ${attr[1]} ${attr[2]}`}
+      className={`imgText__wrap scroll ${props.attr[0]} ${props.attr[1]} ${props.attr[2]}`}
     >
       <h2 className="blind">유용한 사이트 살펴보기</h2>
-      <div className={`imgtext__inner ${attr[3]}`}>
+      <div className={`imgtext__inner ${props.attr[3]}`}>
         <div className="imgText__txt">
-          <span>초보 다.꾸인을 위해</span>
+          <span>{imgTextTitle.subTitle}</span>
+          <h3>{imgTextTitle.title}</h3>
+          <p>{imgTextTitle.desc}</p>
+          {/* <span>초보 다.꾸인을 위해</span>
           <h3>유용한 사이트 살펴보기</h3>
-          <p>다이어리 꾸미기를 처음 할 때 입문용으로 유용한 사이트입니다.</p>
+          <p>다이어리 꾸미기를 처음 할 때 입문용으로 유용한 사이트입니다.</p> */}
           <ul>
-            <li>
+            {
+              imgTextDesc.map((info) => (
+                <ImgTextInner
+                link={info.link}
+                text={info.text}
+                />
+              ))
+            }
+            {/* <li>
               <a href="/">튜토리얼 사이트</a>
             </li>
             <li>
@@ -30,17 +106,28 @@ function ImgText({ attr }) {
             </li>
             <li>
               <a href="/">Youtube 사이트</a>
-            </li>
+            </li> */}
           </ul>
         </div>
-        <div className="imgText__img img1">
+        {
+          imgTextBtn.map((info) => (
+            <ImgTextInner2
+            btnClassName={info.btnClassName}
+            imgClassName={info.imgClassName}
+            btnLink={info.btnLink}
+            btnClass={info.btnClass}
+            linkText={info.linkText}
+            />
+          ))
+        }
+        {/* <div className="imgText__img img1">
           <a href="/">레퍼런스 사이트</a>
         </div>
         <div className="imgText__img img2">
           <a href="/" className="blue">
             튜토리얼 사이트
           </a>
-        </div>
+        </div> */}
       </div>
     </section>
   );
